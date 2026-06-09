6aea759a6fe65cd0a515f7d225d5e5c6c44e9809
Isabella Moreno <imorenom1@unicartagena.edu.co>
2026-06-09 16:54:34 -0500

    Agregar funcionalidad descuento 15%

---
+++ src/Descuento15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Descuento15 {
+    public static void aplicarDescuento15(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa el precio total: ");
+        double precio = Scanner.nextDouble();
+        double descuento = precio * 0.15;
+        double precioFinal = precio - descuento;
+        System.out.println("Descuento del 15%: $" + descuento);
+        System.out.println("*****************************");
+        System.out.println("Precio final: $" + precioFinal);
+        System.out.println("*****************************+")
+    }
+}
